2fa3184fcb889a83a70d95566aecbca893c4fd4e
Colin Clarkson <colinsclarkson@gmail.com>
2016-11-03 18:31:15 -0500

    incompleted states router.use() requires middleware

--- app.js
+++ app.js
@@ -1,10 +1,33 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/task-list/items');
 
+// requiring path module
+const path = require('path');
+
+// route
+const items = require('./routes/items');
+
+//initialize our app
 const app = express();
 
-app.user(bodyParser.json());
-app.user(bodyParser.urlencoded({ extended: false }));
+// set our view directory
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine','ejs');
 
-const port = 3000;
-app.listen(port, () => console.log(`Listening on ${port}`));
+//add our middleware
+app.use(bodyParser.urlencoded({ extended:false }));
+
+//require ItemModel
+const ItemModel = require('./models/ItemModel.js');
+
+// set our routes
+app.use('/items', items);
+app.use('/*', function(req, res, next) {
+  res.redirect('/items');
+});
+
+const port = 3001
+app.listen(port, () => console.log(`Server listening on: ${port}`));
